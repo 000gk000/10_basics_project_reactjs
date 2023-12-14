e18c95877537748927f1a2b0b9c0966027ddcdab
Gopal <sivagk314@gmail.com>
2023-12-14 23:37:35 +0530

    how are you

--- project02/src/components/Button/Button.jsx
+++ project02/src/components/Button/Button.jsx
@@ -2,12 +2,14 @@ import styles from "./Button.module.css"
 import { MdMessage } from "react-icons/md";
  const Button = (props) => {
     return (
-        <button className = {styles.primary_btn}>
+        <button className = { props.isOutline?styles.outline_btn:styles.primary_btn}>
           {props.icon}
           {props.text}
         </button>
        
+       
     )
+    
 }
 
 export default Button
--- project02/src/components/ContactForm/ContactForm.jsx
+++ project02/src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,29 @@
 import { MdMessage } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
-import Button from "../Button/Button"
- import styles from "./ContactForm.module.css"
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
+import Button from "../Button/Button";
+import styles from "./ContactForm.module.css";
 const ContactForm = () => {
-    return (
-        <section className={styles.container}>
-            <div className={styles.contact_form}>
-                <Button  text="VIA CHAT SUPPORT"
-                icon={<MdMessage/>} />
-                <Button  text="VIA CALL"
-                icon={<FaPhoneAlt />} />
-                
-            </div>
-            <div className={styles.contact_image}>
+  return (
+    <section className={styles.container}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            
+            text="VIA CHAT SUPPORT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+        </div>
 
-            </div>
-        </section>
-    )
-}
-export default ContactForm
+        <Button
+        isOutline={true}
+          text=" VIA EMAIL"
+          icon={<MdOutlineMarkEmailUnread fontSize="24px" />}
+        />
+      </div>
+      <div className={styles.contact_image}></div>
+    </section>
+  );
+};
+export default ContactForm;
